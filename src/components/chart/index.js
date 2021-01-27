60c56baf1563066021800534c36e4349e866b0d9
Kurtis Dunn <kdunn@Kurtiss-MacBook-Pro.local>
2021-01-27 15:15:57 +1100

    working demo chart

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './index.scss';
+// import './index.scss';
 import moment from 'moment';
 import  Chart from 'chart.js';
 import 'chartjs-plugin-streaming';
 
-// var ctx = document.getElementById('myChart').getContext('2d');
 
-// var chart = new Chart(ctx, {
+var chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
+function randomScalingFactor() {
+	return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+}
+
+
+
+
 
 
 
@@ -16,36 +31,118 @@ export default class LineChart extends React.Component {
   }
   componentDidMount(){
     console.log('test');
-    const ctx = document.getElementById(this.props.id).getContext('2d');
-    var chart = new Chart(ctx, {
+
+    var color = Chart.helpers.color;
+    var config = {
       type: 'line',
       data: {
         datasets: [{
+          label: 'Dataset 1 (linear interpolation)',
+          backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+          borderColor: chartColors.red,
+          fill: false,
+          lineTension: 0,
+          borderDash: [8, 4],
           data: []
         }, {
+          label: 'Dataset 2 (cubic interpolation)',
+          backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
+          borderColor: chartColors.blue,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
           data: []
         }]
       },
       options: {
+        title: {
+          display: true,
+          text: 'Line chart (hotizontal scroll) sample'
+        },
         scales: {
           xAxes: [{
+            type: 'realtime',
             realtime: {
-              onRefresh: function(chart) {
-                chart.data.datasets.forEach(function(dataset) {
-                  dataset.data.push({
-                    x: Date.now(),
-                    y: Math.random()
-                  });
-                });
-              }
+              duration: 20000,
+              refresh: 1000,
+              delay: 2000,
+              onRefresh: onRefresh
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'value'
+            }
           }]
+        },
+        tooltips: {
+          mode: 'nearest',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: false
         }
       }
+    };
+
+    function onRefresh(chart) {
+      chart.config.data.datasets.forEach(function(dataset) {
+        dataset.data.push({
+          x: Date.now(),
+          y: randomScalingFactor()
+        });
+      });
+    }
+    var ctx = document.getElementById(this.props.id).getContext('2d');
+    window.myChart = new Chart(ctx, config);
+
+    document.getElementById('addDataset').addEventListener('click', function() {
+      var colorName = colorNames[config.data.datasets.length % colorNames.length];
+      var newColor = chartColors[colorName];
+      var newDataset = {
+        label: 'Dataset ' + (config.data.datasets.length + 1),
+        backgroundColor: color(newColor).alpha(0.5).rgbString(),
+        borderColor: newColor,
+        fill: false,
+        lineTension: 0,
+        data: []
+      };
+      config.data.datasets.push(newDataset);
+      window.myChart.update();
     });
+    
+    document.getElementById('removeDataset').addEventListener('click', function() {
+      config.data.datasets.pop();
+      window.myChart.update();
+    });
+    
+    document.getElementById('addData').addEventListener('click', function() {
+      onRefresh(window.myChart);
+      window.myChart.update();
+    });
+    document.getElementById('randomizeData').addEventListener('click', function() {
+      config.data.datasets.forEach(function(dataset) {
+        dataset.data.forEach(function(dataObj) {
+          dataObj.y = randomScalingFactor();
+        });
+      });
+      window.myChart.update();
+    });
+    
+    var colorNames = Object.keys(chartColors);
   }
   render() {
     return (
+      <div>
         <canvas id={this.props.id}></canvas>
+        <p className="text-center">
+          <button type="button" className="btn btn-outline-primary btn-sm" id="randomizeData">Randomize Data</button>
+          <button type="button" className="btn btn-outline-primary btn-sm" id="addDataset">Add Dataset</button>
+          <button type="button" className="btn btn-outline-primary btn-sm" id="removeDataset">Remove Dataset</button>
+          <button type="button" className="btn btn-outline-primary btn-sm" id="addData">Add Data</button>
+        </p>
+      </div>
     );
   }
 }
